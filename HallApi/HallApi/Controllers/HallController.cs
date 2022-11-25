3cf9dc7f09ed9b41eaccf383ab8bceae5f61788d
Oussama <amri.oussama.pro@gmail.com>
2022-11-25 14:25:31 +0100

    CreatApi

---
+++ HallApi/HallApi/Controllers/HallController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HallApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HallController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>() { "Salle 1", "Salle 2" , "Salle 3" };
+        }
+    }
+}
