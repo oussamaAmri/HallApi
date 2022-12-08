8418b3d9f4b6272fc018788e4df96048c06885a3
Kevin Sousselier <kevin.sousselier@gmail.com>
2022-12-08 20:56:44 +0100

    Extension methods

---
+++ HallApi/HallApi/Examples/ExtensionMethod.cs
@@ -0,0 +1,50 @@
+﻿namespace HallApi.Examples
+{
+    public class ExtensionMethod
+    {
+        public void Execute()
+        {
+            var team = new Team
+            {
+                Id = 1,
+                Sport = "Football"
+            };
+
+            var myVar1 = team.IsFootball_1();
+            var myVar2 = ExtensionForTeam.IsFootball_2(team);
+        }
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Sport { get; set; } = string.Empty;
+    }
+
+    public static class ExtensionForTeam
+    {
+        public static bool IsFootball_1(this Team team)
+        {
+            if (team.Sport == "Football")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsFootball_2(Team team)
+        {
+            if (team.Sport == "Football")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
